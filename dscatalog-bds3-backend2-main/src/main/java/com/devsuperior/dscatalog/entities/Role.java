61502663dd567e14ceb37be5a991a6f51a326cb1
Jonath Oliveira <akiles22a@gmail.com>
2022-12-27 12:44:57 -0300

    ORM Mapeada, e seeding de dados incluido

---
+++ dscatalog-bds3-backend2-main/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -0,0 +1,66 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String Authority;
+	
+	public Role () {
+	}
+
+	public Role(long id, String authority) {
+		super();
+		this.id = id;
+		Authority = authority;
+	}
+		
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return Authority;
+	}
+
+	public void setAuthority(String authority) {
+		Authority = authority;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return id == other.id;
+	}
+
+	
+}
